88e0a7025f1f9ffbf2be3e215d7efb48d00cc5ef
MiguelIGP23 <minglesg2401@educantabria.es>
2025-04-30 15:56:41 +0200

    Prueba

--- RETO/src/main/java/com/mycompany/reto/RETO.java
+++ RETO/src/main/java/com/mycompany/reto/RETO.java
@@ -12,6 +12,6 @@ public class RETO {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Prueba de cambio en netbeans");
+        System.out.println("Prueba de cam");
     }
 }
