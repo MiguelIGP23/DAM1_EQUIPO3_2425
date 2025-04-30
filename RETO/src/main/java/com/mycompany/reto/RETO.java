5d222042140dff44561a64f93823f2ca4cbcffa4
DAM123 <DAM123@IF01-14.iesmhp.local>
2025-04-30 15:09:54 +0200

    Creacion del proyecto base

---
+++ RETO/src/main/java/com/mycompany/reto/RETO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.reto;
+
+/**
+ *
+ * @author DAM123
+ */
+public class RETO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
