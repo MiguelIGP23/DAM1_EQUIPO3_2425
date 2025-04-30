97804b5278327027a9d8ca913ab1559139f970af
MiguelIGP23 <minglesg2401@educantabria.es>
2025-04-30 16:14:05 +0200

    Creacion del proyecto JAVA

---
+++ Reto_equipo3/src/main/java/com/mycompany/reto_equipo3/Reto_equipo3.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.reto_equipo3;
+
+/**
+ *
+ * @author DAM123
+ */
+public class Reto_equipo3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Prueba union repo");
+    }
+}
