89dd7dbbc389289b1c82c4abaae2b085cb2accac
Manuentornos1900 <mmediavillaa02@educantabria.es>
2025-05-05 18:46:02 +0200

    Update Teclado.java

--- Reto_equipo3/src/main/java/com/mycompany/reto_equipo3/Validaciones/Teclado.java
+++ Reto_equipo3/src/main/java/com/mycompany/reto_equipo3/Validaciones/Teclado.java
@@ -118,4 +118,125 @@ public class Teclado {
         } while (!valido);
         return rol;
     }
+    //Enum de tipos clase PuntosInteres
+    public static Tipo validatipo(String texto) {
+        boolean valido = false;
+        String tipos = null;
+        Tipo tipo = null;
+        do {
+            try {
+
+                System.out.println(texto);
+               tipos = new Scanner(System.in).nextLine().toLowerCase();
+                switch (tipos) {
+                    case "area de descanso" ->
+                        tipo = tipo.area_de_descanso;
+                    case "botanico" ->
+                       tipo=tipo.botanico;
+                    case "cultural" ->
+                      tipo=tipo.cultural;
+                    case "fauna especifica" ->
+                       tipo=tipo.fauna_especifica;
+                       case "geologico" ->
+                       tipo=tipo.geológico;
+                    case "historico-arqueologico" ->
+                      tipo=tipo.histórico_arqueologico;
+                    case "mirador" ->
+                       tipo=tipo.mirador;
+                       case "naturaleza" ->
+                       tipo=tipo.naturaleza;
+                    case "puntos de agua" ->
+                      tipo=tipo.punto_de_agua;
+                    case "refugio o alojamiento" ->
+                       tipo=tipo.refugio_o_alojamiento;
+                    default ->
+                        throw new Exception("Introduce un tipo valido histórico_arqueologico, " +
+"    naturaleza, mirador," +
+"    area de descanso, punto de agua, refugio o alojamiento, cultural,  geologico, fauna especifica, botanico");
+                }
+
+            } catch (Exception ex) {
+                ex.getMessage();
+            }
+        } while (!valido);
+        return tipo;
+    }
+     public static double validaCoordenadas(int valor){
+        boolean valido=false;
+        double latitud=0;
+        do{
+            try{
+                latitud= new Scanner(System.in).nextDouble();
+            }catch (ArithmeticException e){
+                System.out.println("ERROR: el valor introducido debe ser numerico");
+            }
+        }while(!valido);
+        return latitud;
+    }
+     public static int gravedad(int valor) {
+        boolean valido = false;
+        int gravedad = 0;
+        do {
+            try {
+
+               
+                gravedad= new Scanner(System.in).nextInt();
+                if (gravedad>=1 && gravedad<=5) {
+                    System.out.println("Gravedad correcta");
+                     valido = true;
+                }else{
+                    System.out.println("Introduce un valor correcto del 1 al 5");
+                }
+                
+
+            } catch (Exception a) {
+                System.out.println(a.getMessage());
+            }
+        } while (!valido);
+        return gravedad;
+    }
+     public static String validanombrePuntosInteres(String texto) {
+        boolean valido = false;
+        String nombre = null;
+        do {
+            try {
+                System.out.println(texto);
+                nombre = new Scanner(System.in).nextLine();
+                if (!nombre.matches("[A-ZÑ][A-Za-zÑñáéíóúÁÉÍÓÚ ]{2,40}")) {
+                    throw new Exception("Introduce un nombre valido de maximo de 40caracteres");
+                }
+                valido = true;
+
+            } catch (Exception a) {
+                System.out.println(a.getMessage());
+            }
+        } while (!valido);
+        return nombre;
+    }
+     public static int validatimeestamp(int valor) {
+         boolean valido = false;
+        int tiempo = 0;
+        do {
+            try {
+
+               
+                tiempo= new Scanner(System.in).nextInt();
+                if (tiempo>=0) {
+                    System.out.println("Tiempo de ruta correcto");
+                     valido = true;
+                }else{
+                    System.out.println("Introduce un valor correcto mayor que 0 en este caso");
+                }
+                
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Introduce un valor positivo");
+            }
+            
+        } while (!valido);
+        return tiempo;
+     }
+     
+}
+
 }
