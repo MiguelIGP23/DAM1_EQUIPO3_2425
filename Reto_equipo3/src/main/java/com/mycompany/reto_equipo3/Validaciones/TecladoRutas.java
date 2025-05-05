3d5ebe50a9756c83f23622423fcce2fdf499aa38
MiguelIGP23 <minglesg2401@educantabria.es>
2025-05-05 22:18:56 +0200

    Implementacion de metodo para validar el objeto Set<String>

--- Reto_equipo3/src/main/java/com/mycompany/reto_equipo3/Validaciones/TecladoRutas.java
+++ Reto_equipo3/src/main/java/com/mycompany/reto_equipo3/Validaciones/TecladoRutas.java
@@ -11,15 +11,17 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import com.mycompany.reto_equipo3.Enums.Clasificacion;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 /**
  *
  * Clase dedicada a realizar las validaciones de la clase Rutas
- * 
+ *
  * @author DAM123
  */
 public class TecladoRutas {
-    
+
     //Valida que nombre de ruta tenga 20 caracteres y comience por mayuscula
     public static String validanombre(String texto) {
         boolean valido = false;
@@ -39,7 +41,7 @@ public class TecladoRutas {
         } while (!valido);
         return nombre;
     }
-    
+
     //Valida que nombre de inicio ruta y final ruta tenga 45 caracteres
     public static String validaInicioFinalRuta(String texto) {
         boolean valido = false;
@@ -59,125 +61,153 @@ public class TecladoRutas {
         } while (!valido);
         return nombre;
     }
-    
+
     //Comprueba que latitud y longitud sean numericos
-    public static double validaCoordenadas(String texto){
-        boolean valido=false;
-        double latitud=0;
-        do{
-            try{
+    public static double validaCoordenadas(String texto) {
+        boolean valido = false;
+        double latitud = 0;
+        do {
+            try {
                 System.out.println(texto);
-                latitud= new Scanner(System.in).nextDouble();
-                valido=true;
-            }catch (ArithmeticException e){
+                latitud = new Scanner(System.in).nextDouble();
+                valido = true;
+            } catch (ArithmeticException e) {
                 System.out.println("ERROR: el valor introducido debe ser numerico");
             }
-        }while(!valido);
+        } while (!valido);
         return latitud;
     }
-    
+
     //Comrpueba que la duracion siga el patron dado {hh:mm:ss}
-    public static LocalTime validaDuracion(String texto){
-        boolean valido=false;
-        LocalTime duracion=null;
-        do{
-            try{
+    public static LocalTime validaDuracion(String texto) {
+        boolean valido = false;
+        LocalTime duracion = null;
+        do {
+            try {
                 System.out.println(texto);
-                DateTimeFormatter f=DateTimeFormatter.ofPattern("hh:mm:ss");
-                duracion= LocalTime.parse(new Scanner(System.in).nextLine(),f);
-                valido=true;
-            }catch(DateTimeParseException e){
+                DateTimeFormatter f = DateTimeFormatter.ofPattern("hh:mm:ss");
+                duracion = LocalTime.parse(new Scanner(System.in).nextLine(), f);
+                valido = true;
+            } catch (DateTimeParseException e) {
                 System.out.println("ERROR: foramto no valido");
             }
-        }while(!valido);
+        } while (!valido);
         return duracion;
     }
-    
+
     //Comprueba que el desnivel positivo sea mayor que 0
-    public static int validaDesnivelPositivo(String texto){
-        boolean valido=false;
-        int desnivelPositivo=0;
-        do{
-            try{
+    public static int validaDesnivelPositivo(String texto) {
+        boolean valido = false;
+        int desnivelPositivo = 0;
+        do {
+            try {
                 System.out.println(texto);
-                desnivelPositivo=new Scanner(System.in).nextInt();
-                if(desnivelPositivo<=0){
+                desnivelPositivo = new Scanner(System.in).nextInt();
+                if (desnivelPositivo <= 0) {
                     throw new Exception("ERROR: el numero debe ser positivo");
-                }else{
-                    valido=true;
+                } else {
+                    valido = true;
                 }
-            }catch(InputMismatchException e){
+            } catch (InputMismatchException e) {
                 System.out.println("ERROR: el valor introducido debe ser numerico y entero");
-            }catch(Exception e){
+            } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
-        }while(!valido);
+        } while (!valido);
         return desnivelPositivo;
     }
-    
+
     //Comprueba que el desnivel positivo sea mayor que 0
-    public static int validaDesnivelNegativo(String texto){
-        boolean valido=false;
-        int desnivelNegativo=0;
-        do{
-            try{
+    public static int validaDesnivelNegativo(String texto) {
+        boolean valido = false;
+        int desnivelNegativo = 0;
+        do {
+            try {
                 System.out.println(texto);
-                desnivelNegativo=new Scanner(System.in).nextInt();
-                if(desnivelNegativo>=0){
+                desnivelNegativo = new Scanner(System.in).nextInt();
+                if (desnivelNegativo >= 0) {
                     throw new Exception("ERROR: el numero debe ser negativo");
-                }else{
-                    valido=true;
+                } else {
+                    valido = true;
                 }
-            }catch(InputMismatchException e){
+            } catch (InputMismatchException e) {
                 System.out.println("ERROR: el valor introducido debe ser numerico y entero");
-            }catch(Exception e){
+            } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
-        }while(!valido);
+        } while (!valido);
         return desnivelNegativo;
     }
-    
+
     //Comprueba que la clasificacion tenga uno de los valores dados en el enum
-    public static Clasificacion validaClasificacion(String texto){
-        boolean valido=false;
-        Clasificacion clasificacion=null;
-        do{
+    public static Clasificacion validaClasificacion(String texto) {
+        boolean valido = false;
+        Clasificacion clasificacion = null;
+        do {
             System.out.println(texto);
-            String clas=new Scanner(System.in).nextLine().toLowerCase();
+            String clas = new Scanner(System.in).nextLine().toLowerCase();
             switch (clas) {
-                case "circular"-> {
-                    clasificacion=Clasificacion.CIRCULAR;
+                case "circular" -> {
+                    clasificacion = Clasificacion.CIRCULAR;
                 }
-                case "lineal"-> {
-                    clasificacion=Clasificacion.LINEAL;
+                case "lineal" -> {
+                    clasificacion = Clasificacion.LINEAL;
                 }
-                default -> {System.out.println("ERROR: valor no valido, elige circular o lineal");}
+                default -> {
+                    System.out.println("ERROR: valor no valido, elige circular o lineal");
+                }
             }
-        }while(!valido);
+        } while (!valido);
         return clasificacion;
     }
-    
+
     //Comprueba que el valor introducido este entre 1 y 5
-    public static int validaRango(String texto){
-        boolean valido=false;
-        int valor=0;
-        do{
-            try{
+    public static int validaRango1a5(String texto) {
+        boolean valido = false;
+        int valor = 0;
+        do {
+            try {
                 System.out.println(texto);
-                valor=new Scanner(System.in).nextInt();
-                if(valor<1 || valor>5){
+                valor = new Scanner(System.in).nextInt();
+                if (valor < 1 || valor > 5) {
                     throw new Exception("ERROR: el valor debe estar entre 1 y 5");
-                }else{
-                    valido=true;
+                } else {
+                    valido = true;
                 }
-            }catch(InputMismatchException e){
+            } catch (InputMismatchException e) {
                 System.out.println("ERROR: el valor debe ser numerico");
-            }catch(Exception e){
+            } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
-        }while(!valido);
+        } while (!valido);
         return valor;
     }
-    
+
     //Buscar como implementar una validacion para la clase Set<String>
+    public static Set<String> validaTemporada() {
+        Set<String> temporadasValidas = Set.of("primavera", "verano", "otoño", "invierno");
+        boolean valido = false;
+        Set<String> temporadas = new LinkedHashSet<>();
+        System.out.println("Introduce una o mas temporadas separadas por comas (primevera,verano,otoño,invierno)");
+        do {
+            String temp = new Scanner(System.in).nextLine();
+            //Separamos por las comas
+            String[] partes = temp.split(",");
+            //Lo guardamos en el Set<String>
+            for (String parte : partes) {
+                temporadas.add(parte.trim().toLowerCase());
+            }
+            //Copiamos la lista de temporadas a otro Set<String> y eliminamos las estaciones validas
+            Set<String> noValidas = new LinkedHashSet<>(temporadas);
+            noValidas.removeAll(temporadasValidas);
+            //Comprobamos si queda alguna temporada en la lista
+            if (noValidas.isEmpty()) {
+                valido = true;
+            } else {
+                System.out.println("ERROR: temporada no valida (primavera,verano,otoño,invierno");
+            }
+        } while (!valido);
+        return temporadas;
+    }
+
 }
