8cb7bd6edf71ee655b36dd880b6986ddb4b20d63
Hugo Fernandez <hfernandezc02@educantabria.es>
2025-05-14 19:26:30 +0200

    JavaDoc de DAO actividades

--- Reto_equipo3/src/main/java/com/mycompany/reto_equipo3/DAOS/DAOActividad.java
+++ Reto_equipo3/src/main/java/com/mycompany/reto_equipo3/DAOS/DAOActividad.java
@@ -16,20 +16,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
- *
- *
- * @author DAM119
- *
+ * Clase DAO de Actividad para las consultas/modificaciones simples de la base de datos
+ * @author Hugo Fernández, JavaDoc por Hugo Fernández  
  */
 public class DAOActividad implements InterfazDAO<Actividad> {
 
     private Connection conn;
-
+    /**
+     * Constructor por defecto, devuelve la conexión de la base de datos desde el atributo "conn" con los métodos de acceso de la clase
+     */
     public DAOActividad() {
         this.conn = AccesoABaseDatos.getInstance().getConnexion();
     }
-
+    /**
+     * Método insertar para insertar a la base de datos una actividad
+     * @param actividad parámetro que sirve para llamar a dichos getters de la clase Actividad
+     * @param ruta parámetro que sirve para llamar a dichos getters de la clase Rutas
+     * //@throws SQLException lanzará un mensaje de error en la insercción de una actividad en SQL
+     * //@throws Exception Si el executeUpdate no es 1, no se creará la actividad
+     */
     public void insertar(Actividad actividad, Rutas ruta) {
         String sql = "insert into actividad (nombre, rutas_idRuta) values (?, ?)";
         try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -44,7 +49,12 @@ public class DAOActividad implements InterfazDAO<Actividad> {
             System.out.println(e.getMessage());
         }
     }
-
+    /**
+     * Método modificar para actualizar una actividad de la base de datos
+     * @param actividad parámetro que sirve para llamar a dichos getters de la clase Actividad
+     * //@throws SQLException lanzará un mensaje de error en la modificación de una actividad en SQL
+     * //@throws Exception Si el executeUpdate no es 1, no se modificará la actividad
+     */
     @Override
     public void modificar(Actividad actividad) {
         String sql = "update actividad set nombre = ? where nombre = ?";
@@ -60,7 +70,13 @@ public class DAOActividad implements InterfazDAO<Actividad> {
             System.out.println(e.getMessage());
         }
     }
-
+    /**
+     * Método listar para listar en una colección de List de Actividad todas las actividades de la base de datos
+     * @param id parámetro para usar en el PreparedStatement con la ? y saber si hay o no elementos en la colección con un select. Comienza por 1 para saber si existe alguna actividad
+     * @return la colección listada en List
+     * //@throws SQLException lanzará un mensaje de error cuando se trató de listar una actividad del SQL
+     * //@throws Exception Si el executeUpdate no es 1, no se listará la actividad
+     */
     public List<Actividad> listar(int id) {
         List<Actividad> actividades = new ArrayList<>();
         String sql = "select idActividad, nombre from actividad where rutas_idRuta=? ";
@@ -83,7 +99,12 @@ public class DAOActividad implements InterfazDAO<Actividad> {
         }
         return actividades;
     }
-
+    /**
+     * Método eliminar para eliminar una actividad de la base de datos
+     * @param nombre parámetro para encontrar la actividad deseada para borrar desde el delete
+     * //@throws SQLException lanzará un mensaje de error en la eliminación de una actividad en SQL
+     * //@throws Exception Si el executeUpdate no es 1, no se eliminará la actividad
+     */
     @Override
     public void eliminar(String nombre) {
         String sql = "delete from actividad where nombre = ?";
@@ -98,7 +119,11 @@ public class DAOActividad implements InterfazDAO<Actividad> {
             System.out.println(e.getMessage());
         }
     }
-
+    /**
+     * Método buscar para buscar una actividad de manera rápida
+     * @param nombre parámetro por el que se encontrará desde la consulta select, es decir, con el nombre
+     * @return un objeto Actividad, es decir, la actividad encontrada
+     */
     @Override
     public Actividad buscar(String nombre) {
         Actividad actividad = null;
@@ -115,9 +140,13 @@ public class DAOActividad implements InterfazDAO<Actividad> {
         }
         return actividad;
     }
-
+    /**
+     * Método privado crearActividades, exclusivo para el método de listar, donde irá creando actividades recogiéndolas desde la base de datos y luego listarlas en el método de listar
+     * @param rs parámetro de recogida por consulta en el método listar
+     * @return cada objeto Actividad encontrado desde el método listar
+     * @throws SQLException podría ocurrir un error en el SQL al realizar el rs
+     */
     private Actividad crearActividades(final ResultSet rs) throws SQLException {
         return new Actividad(rs.getInt(1), rs.getString(2));
     }
-
 }
